921eec0771de8ee2df53b7b7343e178fdc86bccf
Thomas Oh <toh@schrodinger.com>
2015-01-08 22:57:20 -0500

    started city model

---
+++ models/city_data.py
@@ -0,0 +1,14 @@
+B = 'blue'
+R = 'red'
+Y = 'yellow'
+K = 'black'
+
+CITY_LIST = [('New York', 93534, B),
+             ('Paris', 230498, B)]
+
+CONNECTION_LIST = [('New York', 'Paris'),
+                   ]
+
+
+
+
