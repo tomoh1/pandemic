d66bfe83fd2213ede261a6e4861f9547572ad5c6
Ellen <ln.huang@gmail.com>
2015-01-19 21:15:12 -0500

    Added player methods: treat and directFlight

--- models/players.py
+++ models/players.py
@@ -2,9 +2,20 @@
 
 
 class Player(object):
-    def __init__(self, name, role):
+    def __init__(self, name):
         self.name = name
-        self.current_city = 'Atlanta'
+        self.current_city = None
         self.hand = []
-        self.role = role
 
+    def treat(self, disease):
+        self.current_city.cubes[disease] -= 1
+        disease.returnCubes(1)
+
+    def directFlight(self, city):
+        if city.name in self.hand:
+            index = self.hand.index(city.name)
+            card = self.hand.pop(index)
+            self.current_city = city
+            return card
+        else:
+            return None
--- test.py
+++ test.py
@@ -12,9 +12,10 @@ ONE_QUIET_NIGHT = 'One_Quiet_Night'
 FORECAST = 'Forecast'
 RESILIENT_POPULATION = 'Resilient_Population'
 
-def distributeHand(player_list, player_deck, num_players):
+def distributeHand(player_list, player_deck):
+    num_players = len(player_list)
     for player in player_list:
-        for n in range(hand_sizes[num_players]):
+        for n in range(HAND_SIZES[num_players]):
             card = player_deck.takeTopCard()
             player.hand.append(card)
 
@@ -45,15 +46,27 @@ def setupBoard(infection_deck, infection_discard_pile):
         for x in range(3):
             infectCity(infection_deck, infection_discard_pile, num)
 
+def directFlight(player, city):
+    card = player.directFlight(city)
+    if card is not None:
+        player_discard_pile.addCardToTop(card)
+    else:
+        print 'City card not in hand'
+
 g = cities.CityGraph(city_data.CITY_LIST, city_data.CONNECTION_LIST)
 player_deck = cards.CardDeck()
 player_discard_pile = cards.CardDeck()
 infection_deck = cards.CardDeck()
 infection_discard_pile = cards.CardDeck()
-event_cards = [GOVERNMENT_GRANT, AIRLIFT, ONE_QUIET_NIGHT,
-               FORECAST, RESILIENT_POPULATION]
-hand_sizes = {2: 4, 3: 3, 4: 2}
 
+EVENT_CARDS = [GOVERNMENT_GRANT, AIRLIFT, ONE_QUIET_NIGHT,
+               FORECAST, RESILIENT_POPULATION]
+HAND_SIZES = {2: 4, 3: 3, 4: 2}
+
+player_list = [players.Player('Ellen'), players.Player('Tom')]
+epidemics = 4
+
+"""
 print "Welcome to Pandemic. Will you be able to save humanity?"
 
 num_players = int(raw_input("Enter number of players (up to 4 players): "))
@@ -69,22 +82,25 @@ while num <= num_players:
     player_list.append(p)
 
 epidemics = int(raw_input("Choose your difficulty level (4,5, or 6): "))
-
+"""
 for city in g.cities():
     player_deck.addCardToTop(city.name)
     infection_deck.addCardToTop(city.name)
-for card in event_cards:
+for card in EVENT_CARDS:
     player_deck.addCardToTop(card)
 
 player_deck.shuffle()
 infection_deck.shuffle()
-distributeHand(player_list, player_deck, num_players)
+distributeHand(player_list, player_deck)
 setupPlayerDeck(player_deck, epidemics)
 
 game_state = game.GameState(player_list)
 g.getCity('Atlanta').research_station = True
-
-
-        
 setupBoard(infection_deck, infection_discard_pile)
+
+p1 = player_list[0]
+p2 = player_list[1]
+
+p1.current_city = g.getCity('Atlanta')
+p2.current_city = g.getCity('Atlanta')
 
