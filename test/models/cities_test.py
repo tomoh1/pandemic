917c04e6a88fc3ac15a3abd6bc62c088e1d06cf4
Thomas Oh <thomas.oh@schrodinger.com>
2015-01-19 21:35:12 -0500

    fixed cities_test

--- test/models/cities_test.py
+++ test/models/cities_test.py
@@ -1,13 +1,10 @@
 # Module import
 import os
 import sys
-base_dir = os.path.join(os.getcwd(), '..')
-sys.path.append(base_dir)
-
 
 # Unit testing framework
 import unittest
-from mock import MagicMock, patch, Mock
+from mock import MagicMock, patch
 
 # Modules under test
 from models import cities
@@ -15,7 +12,3 @@ from models import cities
 class TestCity(unittest.TestCase):
     def test_null(self):
         self.assertTrue(True)
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
